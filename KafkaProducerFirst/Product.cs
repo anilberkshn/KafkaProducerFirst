090a2e3b539f2476d537f6edb6dbbd9bb0ac9e8d
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2023-03-06 14:59:27 +0300

    hatasız ilk çalışması

---
+++ KafkaProducerFirst/Product.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace KafkaProducerFirst
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- KafkaProducerFirst/Program.cs
+++ KafkaProducerFirst/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using Confluent.Kafka;
+using KafkaProducerFirst;
+using Newtonsoft.Json;
 
 namespace KafkaProducerFirst
 {
@@ -6,7 +10,43 @@ namespace KafkaProducerFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var topicName = "topic_0";
+            var kafkaBootStrapServers = "pkc-lzvrd.us-west4.gcp.confluent.cloud:9092";
+            var username = "B2GUYKSUAYO56XE2";
+            var password = "dqBOlwAkb+/nkzLOGDkk/hTQsERKWCNbFrtP7P/hgBYFmvhzg7WjCnmFZvga5wPm";
+
+            var kafkaConfig = new ProducerConfig()
+            {
+                BootstrapServers = kafkaBootStrapServers,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.Plain,
+                SaslUsername = username,
+                SaslPassword = password
+            };
+
+            using var producer = new ProducerBuilder<string, string>(kafkaConfig).Build();
+
+            var product = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "firstTest"
+            };
+
+            var jstr = JsonConvert.SerializeObject(product);
+
+            var kafkaMessage = new Message<string, string>
+            {
+                Key = product.Id.ToString(),
+                Value = jstr
+            };
+
+            var result =  producer.ProduceAsync(topicName, kafkaMessage).GetAwaiter().GetResult();
+
+            Console.WriteLine($"Status: {result.Status}");
+    
+
+
         }
+
     }
 }
